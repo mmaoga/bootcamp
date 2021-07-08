efc2c3ab6a54825c125a331adf9fad912eaca75c
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-09 00:36:05 +0300

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,38 @@
+print ("hello, world")
+print ("hi my name is Dennis Manyara")
+print("This is my first code")
+for _ in range(10):
+    print("Hello, World")
+    text = "Hello my world"
+print(text)
+text = "My name is Dennis Maoga Manyara"
+print(text)
+print("hello\n"*3)
+
+print ("hello world")
+
+print('I love {0} and {1}'.format('bread','butter'))
+print('I love {1} and {0}'.format('bread','butter'))
+
+print ('Hi {2}, {1} and {0}'.format ('Alice','Bob','Carol'))
+
+print ("hello world\n"*2)
+
+a= 1
+a= a+a
+a= a+a	
+a= a+a
+#a not a
+a = True
+a = not a
+a = not a
+a = not a
+
+a = 2
+a = a * a
+a = a * a
+a = a * a
+
+a=b=c=17
+
+print (a)
