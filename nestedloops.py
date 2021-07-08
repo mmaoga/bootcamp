1dfff5271c3d554b0050faeef805e3578e3ff3a2
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-09 02:53:22 +0300

    Create nestedloops.py

---
+++ nestedloops.py
@@ -0,0 +1,17 @@
+age = int(input("Please enter your age: "))
+gender = str (input("Please select your gender, (M for Male and F for Female): "))
+if age < 18:
+	if gender == "M":
+		print ("Son")
+	else:
+		print ("Daughter")
+
+elif age >= 18 and age <65:
+	if gender == "M":
+		print ("Father")
+	else:
+		print ("Mother")
+else:
+	if gender == "M":
+	else:
+		print (grandmother)
