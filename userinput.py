60213f5da273eb03d85103f327e7bc778cfd72ce
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-09 00:35:40 +0300

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -1,9 +1,76 @@
-first_name =str(input('Please enter the first name: '))
-second_name =str(input('Please enter the second name: '))
-third_name =str(input('Please enter the third name: '))
+#first_name =str(input('Please enter the first name: '))
+#second_name =str(input('Please enter the second name: '))
+#third_name =str(input('Please enter the third name: '))
 
 #a = "Maggie"
 #b = "Joel"
 #c = "Sharon"
 
-print ("Hi", third_name,",",second_name, "and", first_name )
+#print ("Hi", third_name,",",second_name, "and", first_name )
+
+
+# num1 =int(input('Please enter the first number:'))
+# num2 =int(input('Please enter the second number: '))
+# num3 =int(input('Please enter the third number: '))
+
+# if num1 == num2 == num3:
+# 	print(" EQUAL")
+# else:
+# 	print("NOT EQUAL")
+
+
+
+# j = 0
+# for i in range (0, 10):
+# 	j += i
+
+# 	print (j)
+
+# j = 1
+# for i in range (0, 10):
+# 	j += j
+
+# 	print (j)
+
+# for j in range (0, 10):
+#  	j += j
+#  	print (j)
+
+
+
+# n = 123456789
+# m = 0
+
+# while n != 0:
+# 	m = (10 * m) + (n % 10)
+# 	n //= 10
+
+# 	print (m, n)
+# 	# print (n)
+
+
+f = 0
+g = 1
+
+for i in range (0, 10):
+	f = f + g
+	g = f - g
+
+	print (f)
+
+
+# for variable in range(start, stop):
+# 	statement1
+# 	statement2
+# 	...
+
+
+# variable = start
+# while variable < stop:
+# 	statement1
+# 	statement2
+
+# 	...
+
+# 	variable +=1
+
