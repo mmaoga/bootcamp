61d887c7065d68acdd22aa35c63f776758723510
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-08 22:36:25 +0300

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,9 @@
+first_name =str(input('Please enter the first name: '))
+second_name =str(input('Please enter the second name: '))
+third_name =str(input('Please enter the third name: '))
+
+#a = "Maggie"
+#b = "Joel"
+#c = "Sharon"
+
+print ("Hi", third_name,",",second_name, "and", first_name )
