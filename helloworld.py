c9d1f404c5ff3ed2ede101b32b924e4663aa31da
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-08 22:12:22 +0300

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,39 @@
+print ("hello, world")
+print ("hi my name is Dennis Manyara")
+print("This is my first code")
+for _ in range(10):
+    print("Hello, World")
+    text = "Hello my world"
+print(text)
+text = "My name is Dennis Maoga Manyara"
+print(text)
+print("hello\n"*3)
+
+name = "Dennis M."
+print("Hello, World, This is your one and only",name)
+
+print ('Hi {2}, {1} and {0}'.format ('Alice','Bob','Carol'))
+
+print ('I love {1} more than {0}, how about you?'.format ('singing','coding'))
+
+number = 1+2*3/4*5/5
+print (number)
+
+data = ("Alice", "Bob", "Carol")
+date = ()
+format_string = "Hi %s %s and %s."
+
+print(format_string % data)
+
+from datetime import date
+today = date.today()
+
+print ("Hi", name ,"today's date is", today)
+
+
+
+x = 7
+y = 0
+
+print (x/y)
+
