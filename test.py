0b32ecef255364c0bcd3ef6b7ccf801349fa6631
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-09 20:52:35 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
 
-for j in range (0, 10):
- 	# j += j
- 	print (j)
+# for j in range (0, 10):
+#  	# j += j
+#  	print (j)
 
 
-# for friend in ["Mike", "Dennis", "Maoga"]:
-#  	invitation = "Hi "+friend+". Please come to my party on Saturday!"
-#  	print (invitation)
+# # for friend in ["Mike", "Dennis", "Maoga"]:
+# #  	invitation = "Hi "+friend+". Please come to my party on Saturday!"
+# #  	print (invitation)
 
-i = 0
-while i < 10:
-	i += 1
-	print (i)
+# i = 0
+# while i < 10:
+# 	i += 1
+# 	print (i)
 
-	
+
+
+print ("Welcome")
