df22183d5c6fd88920ae44fb8222a7673f14ab87
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-12 16:49:39 +0300

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,54 @@
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen"]
+# print (x)
+
+
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen"]
+# x = len(cars)
+# print (x)
+
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen"]
+# cars.remove("Volvo") #removing by calling its name
+# print (cars)
+
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen"]
+# cars.pop(0) #removing by calling its index
+# print (cars)
+
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen"]
+# cars.append("Toyota") #adding stuff to the arrray by appending
+# print (cars)
+
+
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen"]
+# cars.pop(2) #removing by calling its index
+# print (cars)
+
+
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen"]
+# cars.clear()
+# print (cars)
+
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen", "Volvo"]
+# x = cars.count("Volvo") # counting
+# print (x)
+
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen", "Volvo"]
+# cars.sort(reverse = True) # sorting
+# print (cars)
+
+
+cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen", "Volvo"]
+cars.sort() # ascending SORTING
+print (cars)
+
+#Quiz number 4 create an array and add stuff.
+#projects are to be completed by the end of the program by 28th July, 2021
