e608928fba603661210d19d1e437b3bd39a3e93d
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-12 13:37:08 +0300

    Update conditionalloops.py

--- conditionalloops.py
+++ conditionalloops.py
@@ -4,6 +4,14 @@
 # 		continue
 # 	print (x)
 
+
+# fruits = ["Apple","Banana","Cherry"]
+# for x in fruits:
+# 	if x == "Apple" :
+# 		break
+# print ( x )
+
+
 #even numbers
 # for num in range(1,100,3): # 1 is the start point, 100 is the end point and 3 is the step point
 # 	if num%2==0:
@@ -12,25 +20,25 @@
 
 #prime numbers
 
-# for num in range(1,100+1):
-# 	if num>1:
-# 		for j in range (2,num):
-# 			if(num % j==0):
-# 				break
-# 		else:
-# 			print(num)
-
-
-num = 407
-
-# num = int(input ("Please Enter a Number: ")
-	if num > 1:
-		for i in range (2,num):
-			if (num % i) == 0:
-				print (num, "is not a prime number")
-				print (i,"times", num//i, "is",num)
+for num in range(1,100+1):
+	if num>1:
+		for j in range (2,num):
+			if(num % j==0):
 				break
 		else:
-			print (num,"is not a prime number")
-	else:
-		prime(num,"is not a prime number")
+			print(num)
+
+
+# num = 407
+
+# # num = int(input ("Please Enter a Number: ")
+# 	if num > 1:
+# 		for i in range (2,num):
+# 			if (num % i) == 0:
+# 				print (num, "is not a prime number")
+# 				print (i,"times", num//i, "is",num)
+# 				break
+# 		else:
+# 			print (num,"is not a prime number")
+# 	else:
+# 		prime(num,"is not a prime number")
