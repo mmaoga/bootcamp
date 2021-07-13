2cdaf2a9d24928c70bb3c2f5ee3201424a8b511a
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-13 23:18:36 +0300

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,61 +1,88 @@
 print ()
 import random
 
+# input("Hello, Press Enter to start.")
+print()
+
 user_input = str(input("Please enter your name: ")).upper()
 
 print ()
 
 print(user_input, ", Welcome to the Rock, Paper, Scissors Game! Here is the winning guide for the game!! \n\n"
-		+"Winning Rules of the game are as follows: \n"
-		+"(1)Rock vs paper->paper wins \n"
-		+"(2)Rock vs scissor->Rock wins \n"
-		+"(3)Paper vs scissor->scissor wins \n\n")
+    +"Winning Rules of the game are as follows: \n"
+    +"(1)Rock vs paper->paper wins \n"
+    +"(2)Rock vs scissor->Rock wins \n"
+    +"(3)Paper vs scissor->scissor wins \n\n")
 
-user_score = 0
-computer_score = 0
-options = ["rock", "paper", "scissors"]
+
+player = 0
+computer = 0
+tie = 0
+game = 1
+
+choices = ["rock", "paper", "scissors"]
 
 while True:
-    user_input = input("Enter your pick (rock, paper, scissors): ").lower()
-    possible_actions = ["rock", "paper", "scissors"]
-    while user_input not in options:
-    	user_input = input("That is not a valid choice. Please try again: ").lower()
-    computer_input = random.choice(possible_actions)
-    print(f"\nYou picked {user_input}, computer picked {computer_input}.\n")
+  random_index = random.randint(0,2)
+  computer_choice = choices[random_index]
 
-    if user_input == computer_input:
-        print(f"Both players picked {user_input}. It's a TIE!")
-    elif user_input == "rock":
-        if computer_input == "scissors":
-            print("You WIN!\n")
-            
-        else:
-            print("You LOSE.\n")
-            
-    elif user_input == "paper":
-        if computer_input == "rock":
-            print("You WIN!\n")
-            
-        else:
-            print("You LOSE.\n")
-            
-    elif user_input == "scissors":
-        if computer_input == "paper":
-            print("You WIN!\n")
-            
-        else:
-        	print("You LOSE.\n")
-        	
-	  print()
-	  print("You have "+str(user_wins)+" wins")
-	  print("The computer has "+str(computer_wins)+" wins")
-	  print()
+  player_choice = input("Rock, Paper, or Scissors? ").lower()
+  while player_choice not in choices:
+    player_choice = input("That is not a valid choice. Please try again: ").lower()
+  
+  print()
+  print("Your choice:", player_choice)
+  print("Computer's choice:", computer_choice)
+  print()
 
-	  repeat = input("Play again? (Y/N) ").lower()
-	  while repeat not in ['y', 'n']:
-	    repeat = input("That is not a valid choice. Please try again: ").lower()
-	  
-	  if repeat == 'n':
-	    break
+  if player_choice == 'rock':
+    if computer_choice == 'rock':
+      print("It's a TIE!")
+      tie+=1
+    elif computer_choice == 'scissors':
+      print("You WIN!")
+      player+=1
+    elif computer_choice == 'paper':
+      print("You LOSE!")
+      computer+=1
+  elif player_choice == 'paper':
+    if computer_choice == 'paper':
+      print("It's a TIE!")
+      tie+=1
+    elif computer_choice == 'rock':
+      print("You WIN!")
+      player+=1
+    elif computer_choice == 'scissors':
+      print("You LOSE!")
+      computer+=1
+  elif player_choice == 'scissors':
+    if computer_choice == 'scissors':
+      print("It's a TIE!")
+      tie+=1
+    elif computer_choice == 'paper':
+      print("You WIN!")
+      player+=1
+    elif computer_choice == 'rock':
+      print("You WIN!")
+      computer+=1
 
-	  print("\n----------------------------\n")
+  print()
+  print(str(user_input) +" has "+str(player)+" winnings!")
+  print("The computer has "+str(computer)+" winnings!")
+  print ("Number of games tied: "+str(tie)+"!")
+  print()
+
+  repeat = input("Would you like to Play again? (Y/N) ").lower()
+  while repeat not in ['y', 'n']:
+    repeat = input("Invalid. Please try again: ").lower()
+  
+  if repeat == 'n':
+    break
+  else:
+    game+=1
+
+  print("\n.........\n"+"ROUND --  " +str(game)+"!"+"\n...........\n")
+  print ()
+
+ 
+
