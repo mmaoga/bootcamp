91ea46ac1813a2fc260c131b23648ece5d5aef31
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-14 22:17:24 +0300

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -30,6 +30,24 @@
 # print (cars)
 
 
+# first_name =str(input('Please enter the first name: '))
+# second_name =str(input('Please enter the second name: '))
+# third_name =str(input('Please enter the third name: '))
+
+# names = [first_name, second_name,third_name]
+
+# names.reverse()
+# print (names)
+
+# cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen", "Volvo"]
+# fruits = ["Apple", "Oranges"]
+# fruits.insert(1, "grapes")
+# x = fruits.copy()
+# cars.pop(1) #removing by calling its index
+
+# cars.extend(fruits)
+# print (cars)
+# print (x)
 
 # cars = ["Ford", "Volvo", "BMW", "Range Rover", "Volkswagen"]
 # cars.clear()
@@ -51,18 +69,45 @@
 # print (cars)
 
 
+
+# import stdio
+
 # a = []
-# for i in range (n):
-# 	a += [1000]
+# for i in range (1000):
+# 	a += [1]
 
-# 	print (a)
+# 	# x = a[1000]
+# 	print(a)
+
+# x = len(a)
+# print (x)
+
+# a = [1, 2, 3]
+# for i in range(10):
+# 	a[0] = i * i
+# 	print(a)
 
 
-a = []
+
+
+# n = 10
+# a = [0, 1]
+# for i in range(2, n):
+# 	a += [a[i-1] + a[i-2]]
+
+# 	print(a)
+
+import stdio
+from array import array
+
+
+a = stdarray.create1D(10, 0)
 for i in range(10):
-	a[i] = i * i
-
-print (a)
+	a[i] = 9 - i
+	for i in range(10):
+		a[i] = a[a[i]]
+		for v in a:
+			stdio.writeln(v)
 
 #Quiz number 4 create an array and add stuff.
 #projects are to be completed by the end of the program by 28th July, 2021
