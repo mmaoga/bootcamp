8aa8fdada4f0022abb2cd118d57f7f15bab963ef
mmaoga <86660167+mmaoga@users.noreply.github.com>
2021-07-16 17:09:08 +0300

    Create functions2.py

---
+++ functions2.py
@@ -0,0 +1,58 @@
+
+# #1. ASSIGN A FUNCTION TO A VARIABLE
+
+# def func (number):
+#     print(number)
+
+# a_variable = func
+
+# a_variable(1)
+
+
+
+#2. A FUNCTION WITH ARGUMENT
+
+print()
+a = int(input("Please enter your first number: "))
+b = int(input("Please enter your second number: "))
+
+def addition(a, b):
+
+        print("The sum of the two numbers is : ", (a + b))
+
+addition(a, b)
+
+
+
+# #3. FUNCTION WITH CONDITIONS 
+# def recursion(k):
+#   if(k > 0):
+#     result = k + recursion(k - 1)
+#     print(result)
+#   else:
+#     result = 0
+#   return result
+
+# print("\n\nThe Result is: ")
+# recursion(10)
+
+
+# #4. FUNCTION WITH ARGUMENT
+# print ()
+# num = int(input("Please enter a number: "))
+
+# def absolute_value(num):
+#     """This function returns the absolute
+#     value of the entered number"""
+
+#     if num >= 0:
+#         return num
+#     else:
+#         return -num
+
+
+# print(absolute_value(num))
+
+
+
+
